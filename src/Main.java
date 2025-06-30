b0d752da2eaa7713b827dd8c7039bb7c506d7666
minding2796 <124860029+minding2796@users.noreply.github.com>
2025-06-30 23:25:49 +0900

    Main.java 업데이트

--- src/Main.java
+++ src/Main.java
@@ -13,5 +13,6 @@ public class Main {
         System.out.println("Hello, Wor10d!");
         System.out.println("Hello, Wor11d!");
         System.out.println("Hello, Wor12d!");
+        System.out.println("Hello, Wor13d!");
     }
 }
